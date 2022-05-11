7e6f0bc604dad3570f6e801a2178a21165dbadaa
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-11 16:13:15 -0300

    tipagem png e componente sucessfeedback

--- src/components/SucessFeedback/index.tsx
+++ src/components/SucessFeedback/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 import { styles } from './styles';
+import sucessImg from '../../assets/sucess.png';
+import { CopyrightRodape } from '../CopyrightRodape';
 
 export function SucessFeedback() {
   return (
     <View style={styles.container}>
+      <Image source={sucessImg} style={styles.image}/>
+      <Text style={styles.title}>Agradeçemos o feedback</Text>
+      
+      <TouchableOpacity style={styles.touch}>
+        <Text style={styles.buttonTitle}>Quero enviar outro</Text>
+      </TouchableOpacity>
 
+      <CopyrightRodape />
     </View>
   );
 }
