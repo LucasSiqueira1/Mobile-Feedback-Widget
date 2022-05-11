a9389e84de001c76edb7d694d4fd12fde48e9008
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-11 15:20:28 -0300

    componente feedbackform

--- src/components/FeedbackForm/index.tsx
+++ src/components/FeedbackForm/index.tsx
@@ -3,9 +3,9 @@ import { View, TextInput, Image, Text, TouchableOpacity } from 'react-native';
 import { ArrowLeft } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 import { FeedbackType } from '../../components/Widget'
 import { feedbackTypes } from '../../utils/feedbackTypes';
-import { theme } from '../../theme';
 
 
 interface FeedbackFormProps {
@@ -29,6 +29,8 @@ export function FeedbackForm({feedbackType}: FeedbackFormProps) {
           </Text>
         </View>
       </View>
+
+      <TextInput multiline style={styles.input} placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..." placeholderTextColor={theme.colors.text_secondary}></TextInput>
     </View>
   );
 }
