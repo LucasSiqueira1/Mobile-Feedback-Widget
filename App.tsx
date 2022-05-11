e1d327690dba965bfa18f6af7ce9d2fefefd4af5
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-11 12:08:38 -0300

    refatoração do projeto

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
       backgroundColor: theme.colors.background,
     }}>
-
+      {/* barra do smartphone */}
+      <StatusBar style="light" backgroundColor='transparent' translucent /> 
       <Widget/>
-      <StatusBar style="light" backgroundColor="transparent" translucent />
     </View>
   );
 }
