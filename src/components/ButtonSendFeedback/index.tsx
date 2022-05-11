8d2202260553d4302189a7ffc47925c6b2a762b6
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-11 15:58:24 -0300

    componente buttonsendfeedback

--- src/components/ButtonSendFeedback/index.tsx
+++ src/components/ButtonSendFeedback/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from 'react-native';
+import { theme } from '../../theme';
 
 import { styles } from './styles';
 
-export function ButtonSendFeedback() {
-  return (
-    <View style={styles.container}>
+interface ButtonSendFeedbackprops extends TouchableOpacityProps {
+  isLoading: boolean;
+}
 
-    </View>
+
+export function ButtonSendFeedback({ isLoading, ...rest }: ButtonSendFeedbackprops) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      {
+        isLoading ?
+          <ActivityIndicator color={theme.colors.text_on_brand_color} />
+          :
+          <Text style={styles.title}>Enviar Feedback</Text>
+      }
+    </TouchableOpacity>
   );
 }
